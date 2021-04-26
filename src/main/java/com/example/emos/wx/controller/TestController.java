5746dcb7fc20d4082bc345d9045df602024a60b0
YIBOWAY <2338129627@qq.com>
2021-04-26 11:09:31 +0800

    1.完成后端验证配置，添加Valid验证功能，创建专门用来检测的form类

--- src/main/java/com/example/emos/wx/controller/TestController.java
+++ src/main/java/com/example/emos/wx/controller/TestController.java
@@ -1,11 +1,12 @@
 package com.example.emos.wx.controller;
 
 import com.example.emos.wx.common.util.R;
+import com.example.emos.wx.controller.form.TestSayHelloForm;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  * 描述:  测试controller
@@ -14,9 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 @Api("测试接口")
 public class TestController {
-    @GetMapping("/sayHello")
+    @PostMapping("/sayHello")
     @ApiOperation("最简单的测试方法")
-    public R sayHello(){
-        return R.ok().put("message","HelloWorld");
+    public R sayHello(@Valid @RequestBody TestSayHelloForm form){
+        return R.ok().put("message","Hello"+form.getName());
     }
 }
