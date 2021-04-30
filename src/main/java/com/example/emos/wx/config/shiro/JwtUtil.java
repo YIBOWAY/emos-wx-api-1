bd74b013a9092b332250890ae94ae51fef24ff83
YIBOWAY <2338129627@qq.com>
2021-04-30 17:02:38 +0800

    1.创建JWT工具类，封装常用方法如：创建令牌，获取用户ID等 2.将令牌封装成认证对象，传递给Realm类 3.创建Realm类，shiro的认证与授权类；提前完成登录与授权功能的框架；

---
+++ src/main/java/com/example/emos/wx/config/shiro/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.example.emos.wx.config.shiro;
+
+import cn.hutool.core.date.DateField;
+import cn.hutool.core.date.DateUtil;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 描述:  JWT工具类
+ */
+@Component
+@Slf4j
+public class JwtUtil {
+    @Value("${emos.jwt.secret}")
+    private String secret;
+
+    @Value("${emos.jwt.expire}")
+    private int expire;
+
+    public String createToken(int userId){
+        Date date = DateUtil.offset(new Date(), DateField.DAY_OF_YEAR,5);//找到偏移5天的日期
+        Algorithm algorithm = Algorithm.HMAC256(secret); //加密算法生成密钥,封装成加密对象
+        JWTCreator.Builder builder = JWT.create();
+        String token = builder.withClaim("userId",userId).withExpiresAt(date).sign(algorithm);//由用户ID，过期日期，加密密钥生成token
+        return token;
+    }
+
+    public int getUserId(String token){
+        DecodedJWT jwt = JWT.decode(token);
+        int userId = jwt.getClaim("userId").asInt();
+        return userId;
+    }
+
+    public void verifierToken(String token){
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        JWTVerifier verifier = JWT.require(algorithm).build();//生成验证对象
+        verifier.verify(token);
+    }
+}
