9c0b96b5f075c7a8855e675dcda9b13e3f11d43b
YIBOWAY <2338129627@qq.com>
2021-05-04 22:43:58 +0800

    1.完成OAuth2Filter类编写； 2.完成shiroConfig类编写；

---
+++ src/main/java/com/example/emos/wx/config/shiro/OAuth2Filter.java
@@ -0,0 +1,140 @@
+package com.example.emos.wx.config.shiro;
+
+
+import cn.hutool.core.util.StrUtil;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import org.apache.http.HttpStatus;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 描述:  过滤器
+ * 拦截下所有的请求，判断是否交给shiro框架处理
+ * 如果OAuth2Filter是单例的，那么多个线程使用OAuth2Filter的时候，用的ThreadLocalToken就是同一个对象，所以会有线程安全问题。
+ */
+@Component
+@Scope("prototype") //设置成多例
+public class OAuth2Filter extends AuthenticatingFilter {
+
+    @Autowired
+    private ThreadLocalToken threadLocalToken;
+
+    @Value("${emos.jwt.cache-expire}")
+    private int cacheExpire;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Override
+    protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) throws Exception { //子类必须继承父类中的纯虚方法
+        HttpServletRequest req = (HttpServletRequest) request;
+        String token = getRequestToken(req);
+        if (StrUtil.isBlank(token)){
+            return null;
+        }
+        return new OAuth2Token(token);
+    }
+
+    @Override
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) { //判断是否交给shiro处理
+        HttpServletRequest req = (HttpServletRequest) request;
+        if (req.getMethod().equals(RequestMethod.OPTIONS.name())){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setHeader("Access-Control-Allow-Credentials","true");
+        resp.setHeader("Access-Control-Allow-Origin",req.getHeader("Origin"));
+        //设置跨域
+
+        threadLocalToken.clear();
+
+        String token = getRequestToken(req);
+        if (StrUtil.isBlank(token)){
+            resp.setStatus(HttpStatus.SC_UNAUTHORIZED);
+            resp.getWriter().print("无效的令牌");
+            return false;
+        }
+        try {
+            jwtUtil.verifierToken(token);
+        }catch (TokenExpiredException e){ //抛出过期日期异常
+            if (redisTemplate.hasKey(token)){
+                redisTemplate.delete(token);
+                int userId = jwtUtil.getUserId(token);
+                token = jwtUtil.createToken(userId);
+                redisTemplate.opsForValue().set(token,userId+"",cacheExpire, TimeUnit.DAYS);
+            }
+            else {
+                resp.setStatus(HttpStatus.SC_UNAUTHORIZED);
+                resp.getWriter().print("令牌已过期");
+                return false;
+            }
+
+        }catch (JWTDecodeException e){ //JWTtoken字符串异常
+            resp.setStatus(HttpStatus.SC_UNAUTHORIZED);
+            resp.getWriter().print("无效令牌");
+            return false;
+
+        }
+        boolean bool = executeLogin(request,response);//间接调用realm类进行认证与授权
+        return bool;
+    }
+
+    @Override
+    protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException e, ServletRequest request, ServletResponse response) {//当认证失败时返回，使返回更有针对性
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setHeader("Access-Control-Allow-Credentials","true");
+        resp.setHeader("Access-Control-Allow-Origin",req.getHeader("Origin"));
+        //设置跨域
+        resp.setStatus(HttpStatus.SC_UNAUTHORIZED);
+        try {
+            resp.getWriter().print(e.getMessage());
+        } catch (Exception exception) {
+
+        }
+        return false;
+    }
+
+    @Override
+    public void doFilterInternal(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        super.doFilterInternal(request, response, chain);
+    }
+
+    private String getRequestToken(HttpServletRequest request){
+        String token = request.getHeader("token");
+        if (StrUtil.isBlank(token)){
+            token = request.getParameter("token");
+        }
+        return token;
+    }
+}
